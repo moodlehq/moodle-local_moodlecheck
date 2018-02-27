f69469424eb393c9a004efaf5df04dea3c0ffa9f
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2018-02-27 11:12:29 +0100

    Issue #31 - Cover with basic unit tests

---
+++ tests/moodlecheck_rules_test.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains helper testcase for testing "moodle" CS Sniffs.
+ *
+ * @package    local_moodlecheck
+ * @subpackage phpunit
+ * @category   phpunit
+ * @copyright  2018 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die(); // Remove this to use me out from Moodle.
+
+
+class local_moodlecheck_rules_testcase extends advanced_testcase {
+
+    public function setUp() {
+        global $CFG;
+        parent::setUp();
+        // Add the moodlecheck machinery.
+        require_once($CFG->dirroot . '/local/moodlecheck/locallib.php');
+        // Load all files from rules directory.
+        if ($dh = opendir($CFG->dirroot. '/local/moodlecheck/rules')) {
+            while (($file = readdir($dh)) !== false) {
+                if ($file != '.' && $file != '..') {
+                    $pathinfo = pathinfo($file);
+                    if (isset($pathinfo['extension']) && $pathinfo['extension'] == 'php') {
+                        require_once($CFG->dirroot. '/local/moodlecheck/rules/'. $file);
+                    }
+                }
+            }
+            closedir($dh);
+        }
+        // Load all rules.
+        local_moodlecheck_registry::enable_all_rules();
+    }
+
+    /**
+     * Verify the ::class constant is not reported as phpdoc problem.
+     */
+    public function test_constantclass() {
+        global $PAGE;
+        $output = $PAGE->get_renderer('local_moodlecheck');
+        $path = new local_moodlecheck_path('local/moodlecheck/tests/fixtures/constantclass.php ', null);
+        $result = $output->display_path($path, 'xml');
+
+        // Convert results to XML Objext.
+        $xmlresult = new DOMDocument();
+        $xmlresult->loadXML($result);
+
+        // Let's verify we have received a xml with file top element and 2 children.
+        $expect = new DOMDocument();
+        $expect->loadXML('<file name="">
+              <error line="" severity="" message="" source=""/>
+              <error line="" severity="" message="" source=""/>
+            </file>');
+        $this->assertEqualXMLStructure($expect->firstChild, $xmlresult->firstChild, true);
+        // Also verify that contents do not include any problem with line 42 / classesdocumented. Use simple string matching here.
+        $this->assertContains('line="20"', $result);
+        $this->assertContains('packagevalid', $result);
+        $this->assertNotContains('line="42"', $result);
+        $this->assertNotContains('classesdocumented', $result);
+    }
+}
