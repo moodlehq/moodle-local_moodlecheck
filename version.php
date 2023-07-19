5406581de77e5d64a579587d263f6846152f05fe
Andrew Nicols <andrew@nicols.co.uk>
2023-07-19 22:57:56 +0800

    Prepare v1.1.7 release

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2023071400;
-$plugin->release   = '1.1.6';
+$plugin->version  = 2023071900;
+$plugin->release   = '1.1.7';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires = 2018051700;
 $plugin->component = 'local_moodlecheck';
